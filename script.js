99a71db7df50a505a23f81a55c4e7a101c7b2ae8
dikshat25 <161052953+dikshat25@users.noreply.github.com>
2026-03-25 01:18:52 +0530

    Fix form validation condition for name input

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 function validateForm() {
     let name = document.getElementById('name').value;
-    if(name == "") {
+
+    if (name === "") {
         alert("Name is required");
+        return false;
     }
+
+    alert("Registration Successful!");
+    return true;
 }
